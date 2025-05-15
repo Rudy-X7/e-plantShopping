3d935776bfda4e049e5e030ac8db4527294ce3fd
Rudy-X7 <rudraksh0301.g@gmail.com>
2025-05-15 16:44:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping_React",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
